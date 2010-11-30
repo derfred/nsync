3fe0c4b19e1a9c91904ab26f26b14805913adbcf
Frederik Fix <fix@tunupa.nld.ds.mpg.de>
2010-11-30 13:38:44 +0100

    started test suite

---
+++ tests/test_base.js
@@ -0,0 +1,42 @@
+var evt_queue;
+
+module("Event Queue", {
+  setup: function() {
+    evt_queue = new EventQueue();
+  }
+});
+
+test("initialization", function() {
+  ok(evt_queue.empty());
+});
+
+test("adding an item", function() {
+  var event = new Event(1, "mike_check");
+  evt_queue.add_event(event);
+  ok(!evt_queue.empty());
+  equals(event, evt_queue.pop_next_event());
+});
+
+test("popping an item", function() {
+  evt_queue.add_event(new Event(1, "mike_check"));
+  evt_queue.pop_next_event();
+  ok(evt_queue.empty());
+});
+
+test("time ordering of items", function() {
+  evt_queue.add_event(new Event(1.5, "mike_check"));
+  evt_queue.add_event(new Event(1, "feedback_cancel"));
+  equals(1, evt_queue.pop_next_event().time);
+});
+
+test("finding an item by predicate", function() {
+  var evt1 = new Event(3, "age_check");
+  var evt2 = new Event(1.5, "mike_check");
+  var evt3 = new Event(2, "age_check");
+
+  evt_queue.add_event(evt1);
+  evt_queue.add_event(evt2);
+  evt_queue.add_event(evt3);
+
+  equals(evt3, evt_queue.pop_next_event(function(e) { return e.type == "age_check"; }));
+});
