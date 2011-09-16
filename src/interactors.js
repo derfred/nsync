889fdabc58f350510a5fdcf833f8fc3b2ba0992e
Frederik Fix <ich@derfred.com>
2011-09-16 12:19:03 +0200

    implemented another interactor

--- src/interactors.js
+++ src/interactors.js
@@ -26,3 +26,20 @@ PostResetPerturber.prototype.event_reset = function(simulator, options) {
 }
 
 export("PostResetPerturber", PostResetPerturber);
+
+
+function PostResetInteractor(neuron, time, callback) {
+  this.neuron = neuron;
+  this.time = time;
+  this.fired = false;
+  this.callback = callback;
+}
+
+PostResetInteractor.prototype.event_reset = function(simulator, options) {
+  if(!this.fired && options.recipient == this.neuron && this.time < simulator.current_time) {
+    this.callback(simulator, this.neuron, options);
+    this.fired = true;
+  }
+}
+
+export("PostResetInteractor", PostResetInteractor);
