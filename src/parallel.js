ab897ec478ba07d430c6ab72a9d421c3c7eed857
Frederik Fix <ich@derfred.com>
2011-12-18 14:47:02 +0100

    add helper for parallelization

---
+++ src/parallel.js
@@ -0,0 +1,45 @@
+var cluster = require('cluster');
+var numCPUs = require('os').cpus().length;
+var workers = [];
+
+exports.parallelize = function(work_object) {
+  if(cluster.isMaster) {
+    for (var i = 0; i < numCPUs; i++) {
+      var worker = cluster.fork()
+      worker.on("message", function(m) {
+        if(m.cmd == "done") {
+          work_object.add_result(m.result);
+        }
+
+        if(m.cmd == "online" || m.cmd == "done") {
+          var worker = workers[m._workerId-1];
+          var param = work_object.next();
+          if(param != undefined) {
+            worker.send({ cmd: "process", param: param })
+          } else {
+            worker.send({ cmd: "quit" })
+          }
+        }
+      });
+      workers.push(worker);
+    }
+    process.on("exit", function() {
+      work_object.complete();
+    });
+  } else {
+    console.log("worker", process.env.NODE_WORKER_ID, "started", process.pid);
+    process.on("message", function(m) {
+      if(m.cmd == "process") {
+        work_object.process(m.param, function(result) {
+          process.send({
+            cmd: "done",
+            result: result,
+            "_workerId": parseInt(process.env.NODE_WORKER_ID)
+          });
+        });
+      } else if(m.cmd == "quit") {
+        process.exit(0);
+      }
+    });
+  }
+}
