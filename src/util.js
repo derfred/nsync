53d23ac998d0e4c6da2dd913f303cbcba8499f83
Frederik Fix <ich@derfred.com>
2012-01-03 10:41:52 +0100

    cargo culted permute function

--- src/util.js
+++ src/util.js
@@ -142,6 +142,25 @@ function array_mean(array) {
 export_sym("array_mean", array_mean);
 
 
+//+ Jonas Raoni Soares Silva
+//@ http://jsfromhell.com/array/permute [rev. #1]
+function array_permute(v){
+  for(var p = -1, j, k, f, r, l = v.length, q = 1, i = l + 1; --i; q *= i);
+
+  for(x = [new Array(l), new Array(l), new Array(l), new Array(l)], j = q, k = l + 1, i = -1;
+        ++i < l; x[2][i] = i, x[1][i] = x[0][i] = j /= --k);
+
+  for(r = new Array(q); ++p < q;)
+    for(r[p] = new Array(l), i = -1; ++i < l; !--x[1][i] && (x[1][i] = x[0][i],
+            x[2][i] = (x[2][i] + 1) % l), r[p][i] = v[x[3][i]])
+      for(x[3][i] = x[2][i], f = 0; !f; f = !f)
+        for(j = i; j; x[3][--j] == x[2][i] && (x[3][i] = x[2][i] = (x[2][i] + 1) % l, f = 1));
+  return r;
+};
+
+export_sym("array_permute", array_permute);
+
+
 function merge(left, right) {
   if(!right) {
     return left;
